b2feb6b5bef88820962d936b9ee7b7c2d4d922ec
Ashish Khawla <khawla@fanaticcoders.com>
2025-09-12 15:57:11 +0530

    forget code reverse the code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -139,20 +139,20 @@ class AuthController extends Controller
             ['token' => $hashedToken, 'created_at' => now()]
         );
     
-            // 👇 Yahan se frontend ka URL banao
-        $resetUrl = env('FRONTEND_URL') . '/reset-password?token=' . $token;
+        // 👇 Yahan se frontend ka URL banao
+        // $resetUrl = env('FRONTEND_URL') . '/reset-password?token=' . $token;
 
-        // Send the URL in the email
-        Mail::send('emails.reset-password', ['url' => $resetUrl], function ($message) use ($request) {
-            $message->to($request->email);
-            $message->subject('Reset Password Notification');
-        });
-
-        // Send the original token in the email
-        // Mail::send('emails.reset-password', ['token' => $token], function ($message) use ($request) {
+        // // Send the URL in the email
+        // Mail::send('emails.reset-password', ['url' => $resetUrl], function ($message) use ($request) {
         //     $message->to($request->email);
         //     $message->subject('Reset Password Notification');
         // });
+
+        // Send the original token in the email
+        Mail::send('emails.reset-password', ['token' => $token], function ($message) use ($request) {
+            $message->to($request->email);
+            $message->subject('Reset Password Notification');
+        });
     
         return response()->json([
             'success' => true,
